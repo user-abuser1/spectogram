61896e0d58d957081346da00f02480b7b162c1c0
user-abuser1 <122151550+user-abuser1@users.noreply.github.com>
2023-08-11 18:30:12 +0300

    Update dependencies.py

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,11 +1,10 @@
-"""
-Сбирает в себе объкты, функции, переменные и методы.
-"""
+"""Collects objects, functions, variables and methods."""
+
 
 import os
-
 from dotenv import load_dotenv
 
-load_dotenv()
 
-APP_TOKEN = os.getenv('TOKEN')
+load_dotenv() # Function for detect and load enviroment file
+
+APP_TOKEN = os.getenv('TOKEN') # Application Token should be in .env
